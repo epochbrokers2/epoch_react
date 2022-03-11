1bd805d4f434fcd99b8550edea36bf9abb6dec34
Mohd Aadil Rana <epochbrokers2@gmail.com>
2022-03-11 12:47:44 +0530

    Routed remove extra code

--- src/Header/Routing.js
+++ src/Header/Routing.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route,useLocation  } from "react-router-dom";
 import "../assets/css/compiled-css/App.css"
 import Header from "./Header";
-import SubRouting from "./SubRouting";
 import Home from "../pages/Home/Home"
 import About from "../pages/About/About";
 import Services from "../pages/About/Services";
@@ -19,27 +18,12 @@ import L_English from "../pages/Language/L_English"
 import L_Hindi from "../pages/Language/L_Hindi"
 
 function Routing({menuItems}) {
-    // console.log(menuItems);   
     
     return (
         <>
             <Router>
                 <Header menuItems={menuItems} />
                 <Routes>
-                    {/* {
-                        menuItems.submenu ? (
-                            <>
-                            {console.log(menuItems)}
-                            <SubRouting submenu = {menuItems.submenu}  />
-                            </>
-                        ) : (
-                            <>
-                               
-                                <Route path={menuItems.url} element={`<${menuItems.component}/>`} />
-                            </>
-                            
-                        )
-                    } */}
                     <Route path="/" element={<Home />} />
                     <Route exact path="/about-us" element={<About />} />
                     <Route  path="/about-us/our-services" element={<Services />} />
@@ -54,8 +38,6 @@ function Routing({menuItems}) {
                     <Route  path="/login" element={<Login />} />
                     <Route  path="/language/en" element={<L_English />} />
                     <Route  path="/language/hi" element={<L_Hindi />} />
-
-                    {/* <Route path="/policies/property/:slug" element={<Policy />} /> */}
                     <Route
                         path="*"
                         element={
